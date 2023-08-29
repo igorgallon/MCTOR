9cdbbd805487d15630a25117cdb8808a8673deb8
igorgallon <igor.felipe.gallon@outlook.com>
2023-08-29 10:07:54 -0300

    Remove 'euclidean' argument from pdist2

--- utils.m
+++ utils.m
@@ -40,7 +40,7 @@ classdef utils
             x = obj;
             y = zeros(height(obj), 2);
             % Calculate the euclidean distance
-            d = pdist2(x, y, 'euclidian');
+            d = pdist2(x, y);
             % Find the shortest distance
             [~, idx] = min(d(:,1));
             % Save the best solution
